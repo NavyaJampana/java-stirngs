4cfe607c7b40451e4163f6e4df5be1bf04550e5c
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 17:02:38 +0530

    Add files via upload

---
+++ StringIndexOf.java
@@ -0,0 +1,9 @@
+package Strings;
+public class StringIndexOf {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        int index = str.indexOf("Program");
+        System.out.println("Index of 'Program': " + index);
+    }
+}
+
