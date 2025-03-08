18953841b4fd51325f47225b5276345b52f7d7fa
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 17:06:38 +0530

    Add files via upload

---
+++ StringSubstring.java
@@ -0,0 +1,8 @@
+package Strings;
+public class StringSubstring {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String subStr = str.substring(6);  // Extract "World"
+        System.out.println("Substring: " + subStr);
+    }
+}
