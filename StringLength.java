6cc3a996575897371c2c1220e17a58f5fa3924a0
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 17:04:00 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,7 @@
+package Strings;
+public class StringLength {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        System.out.println("Length of string: " + str.length());
+    }
+}
