e63151f05a0f1b5ece2a052574bb66c867078bc2
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 17:04:49 +0530

    Add files via upload

---
+++ StringReplace.java
@@ -0,0 +1,7 @@
+package Strings;
+public class StringReplace {
+    public static void main(String[] args) {
+        String str = "Java is fun";
+        System.out.println(str.replace("Java", "Python"));
+    }
+}
