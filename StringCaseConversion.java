bf8c3be6717ab392537b129849a3977bd01957ee
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 17:00:29 +0530

    Add files via upload

---
+++ StringCaseConversion.java
@@ -0,0 +1,8 @@
+package Strings;
+public class StringCaseConversion {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        System.out.println("Uppercase: " + str.toUpperCase());
+        System.out.println("Lowercase: " + str.toLowerCase());
+    }
+}
