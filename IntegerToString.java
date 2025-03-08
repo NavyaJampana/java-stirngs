422f243d1eb61ad5cf6b7b2e9349efa0bf55cd82
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 17:00:53 +0530

    Add files via upload

---
+++ IntegerToString.java
@@ -0,0 +1,9 @@
+package Strings;
+public class IntegerToString {
+    public static void main(String[] args) {
+        Integer num = 123;
+        String str = num.toString();
+        System.out.println("Integer as String: " + str);
+    }
+}
+
